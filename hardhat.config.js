5881a043557ac0d90ca4d6aea3f0cb1171924d68
saifffff <angryn1nj4@gmail.com>
2023-11-05 14:03:35 +0530

    done adding dependencies

--- hardhat.config.js
+++ hardhat.config.js
@@ -2,6 +2,7 @@ require("@nomiclabs/hardhat-waffle");
 
 module.exports = {
   solidity: "0.8.4",
+  // configured hardhat to put the smart contracts at a different location to be able to access the contracts from the client side interface of the application
   paths: {
     artifacts: "./src/backend/artifacts",
     sources: "./src/backend/contracts",
